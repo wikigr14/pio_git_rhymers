cc5258ff30cd42d849023518130b493bf33b6c86
Wiktor Grzyb <251151@edu.p.lodz.pl>
2025-03-10 13:12:30 +0100

    2. Poprawienie formatowania w pliku HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,14 +4,13 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
 
     int totalRejected = 0;
 
-    public int reportRejected() {
-        return totalRejected;
+    public int reportRejected() return totalRejected;
     }
 
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
-            totalRejected++;
+            if (!callCheck() && in > peekaboo())
+                totalRejected++;
             else
                 super.countIn(in);
-    }
+        }
 }
