c95a5d9276825efd33c352f2599c31f11e01fdce
Wiktor Grzyb <251151@edu.p.lodz.pl>
2025-03-31 13:32:42 +0200

    14. Usunięcie niepotrzebnej klasy Node i wcielenie jej w intLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -37,4 +37,15 @@ public class IntLinkedList {
         return ret;
     }
 
+    private static class Node {
+
+        public final int value;
+        public Node prev, next;
+
+        public Node(int i) {
+            value = i;
+        }
+
+    }
+
 }
